0ab63b2eddba71f1be7614605f3ee3cdb25a1941
valcaro87@gmail.com <valcaro87@gmail.com>
2022-07-06 19:43:15 +0800

    updated test data

--- spec/features/seating_spec.rb
+++ spec/features/seating_spec.rb
@@ -7,11 +7,15 @@ describe "User submit inputs", type: :feature do
     end
 
     context "when all inputs are valid" do
-      it "should be successful" do
-        fill_in "Column/Rows:", with: "[[3,2], [4,3], [2,3], [3,4]]"
-        fill_in "Number of passengers:", with: "30"
-        click_button "Submit"
-        expect(page).to have_content("Airplane seats generated")
+      test_data = [[[1, 1],[1, 1],[2,1]],4], [[[1, 1],[1, 1],[1,1]],3], [[[1, 1],[1, 1]],2],[[[2, 1],[1, 8],[1,1]], 11], [[[2, 8],[3, 8],[2,1]],42],[[[3,2], [4,3], [2,3], [3,4]], 30]
+
+      test_data.each do |arr, num_passenger|
+        it "should be successful" do
+          fill_in "Column/Rows:", with: "#{arr}"
+          fill_in "Number of passengers:", with: "#{num_passenger}"
+          click_button "Submit"
+          expect(page).to have_content("Airplane seats generated")
+        end
       end
     end
 
